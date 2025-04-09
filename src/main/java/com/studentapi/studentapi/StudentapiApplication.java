dfcf385ba73d6d43b1f81e20cb98a0b0a3905da0
Gustavo Coimbra <gustavocoimbra04@gmail.com>
2025-04-09 17:37:05 -0300

    Add API Documentation

--- src/main/java/com/studentapi/studentapi/StudentapiApplication.java
+++ src/main/java/com/studentapi/studentapi/StudentapiApplication.java
@@ -1,9 +1,12 @@
 package com.studentapi.studentapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Student's API", description = "Student's API that allow retrieve students and save students in MongoDB", version = "0.0"))
 public class StudentapiApplication {
 
 	public static void main(String[] args) {
