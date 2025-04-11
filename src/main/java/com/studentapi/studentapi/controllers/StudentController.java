da192df8aff403f1ba8974edc7622df68be9752f
Gustavo Coimbra <gustavocoimbra04@gmail.com>
2025-04-11 11:15:55 -0300

    Add more endpoints

--- src/main/java/com/studentapi/studentapi/controllers/StudentController.java
+++ src/main/java/com/studentapi/studentapi/controllers/StudentController.java
@@ -3,6 +3,8 @@ package com.studentapi.studentapi.controllers;
 import com.studentapi.studentapi.models.Student;
 import com.studentapi.studentapi.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,12 +19,24 @@ public class StudentController {
     private StudentService studentService;
 
     @GetMapping
-    public List<Student> getAllStudents() {
-        return studentService.findAll();
+    public ResponseEntity<List<Student>> findAll() {
+        return ResponseEntity.ok(studentService.findAll());
     }
 
     @PostMapping
-    public Student createStudent(@RequestBody Student student) {
-        return studentService.save(student);
+    public ResponseEntity<Student> save(@RequestBody Student student) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(studentService.save(student));
+    }
+
+    @PutMapping
+    public ResponseEntity<Student> update(@RequestBody Student student) {
+        return ResponseEntity.ok(studentService.save(student));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Student> deleteById(@PathVariable("id") String id) {
+        studentService.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
